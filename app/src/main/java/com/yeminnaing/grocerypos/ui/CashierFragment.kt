94ccc8bb73f710710a4b7316114c9e6d4ef9b940
YeMinNaing <109402630+YeMinNaing1112@users.noreply.github.com>
2024-02-03 19:55:32 +0630

    CheckOutItemsUI Finsihed

--- app/src/main/java/com/yeminnaing/grocerypos/ui/CashierFragment.kt
+++ app/src/main/java/com/yeminnaing/grocerypos/ui/CashierFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.yeminnaing.grocerypos.R
 import com.yeminnaing.grocerypos.adapters.CategoryAdapter
 import com.yeminnaing.grocerypos.adapters.ShowItemsForSaleAdapter
 import com.yeminnaing.grocerypos.adapters.ShowStockAlertAdapter
@@ -22,22 +24,29 @@ class CashierFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCashierBinding.inflate(inflater, container, false)
-        val view = binding.root
         // Inflate the layout for this fragment
-        return view
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setUpShowStockView()
         setUpCategoryView()
         setUpShowItemsView()
+        setUpListeners(view)
+    }
+
+    private fun setUpListeners(view: View) {
+        binding.rlProceedToCheckout.setOnClickListener {
+            Navigation.findNavController(view)
+                .navigate(R.id.action_nav_cashier_to_checkOutItemsFragment)
+        }
     }
 
     private fun setUpShowItemsView() {
-        mShowItemsForSaleAdapter= ShowItemsForSaleAdapter()
+        mShowItemsForSaleAdapter = ShowItemsForSaleAdapter()
         binding.rvItemsForSale.also {
-            it.adapter=mShowItemsForSaleAdapter
-            it.layoutManager=LinearLayoutManager(this.context)
+            it.adapter = mShowItemsForSaleAdapter
+            it.layoutManager = LinearLayoutManager(this.context)
         }
     }
 
